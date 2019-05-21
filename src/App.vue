1287af38165be62d36e952d858d86b418adbadf9
liuweiwei <liuweiwei@sinohealth.cn>
2019-05-21 17:06:09 +0800

    vue.js elementUI初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <router-link to="/view1">Go to view1</router-link>
+<router-link to="/view2">Go to view2</router-link>
+<!-- 路由出口 -->
+<!-- 路由匹配到的组件将渲染在这里 -->
+<router-view></router-view>
+    <img src="./assets/logo.png">
+    <router-view/>
+      <firstcomponent></firstcomponent>
+  </div>
+</template>
+<import firstcomponent from './components/first-component.vue'>
+
+<script>
+export default {
+  name: 'App',
+  　　data () {
+　　　　return {
+　　　　　　msg: 'Welcome to Your Vue.js App'
+　　　　}
+　　}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
